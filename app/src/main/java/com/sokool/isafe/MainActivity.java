95dafe2cb67e089f05fdc171c13165ccc5a29826
SokoPaulSokool <sopapaso73@gmail.com>
2019-09-10 11:08:37 +0300

    initial commit with basic ui

---
+++ app/src/main/java/com/sokool/isafe/MainActivity.java
@@ -0,0 +1,75 @@
+package com.sokool.isafe;
+
+import android.os.Bundle;
+import android.support.constraint.Guideline;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    protected Toolbar toolbar;
+    protected Button callForHelp;
+    protected Button budies;
+    protected Button insurance;
+    protected Guideline guideline;
+    protected Guideline guideline2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_main);
+        initView();
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initView() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        callForHelp = (Button) findViewById(R.id.call_for_help);
+        callForHelp.setOnClickListener(MainActivity.this);
+        budies = (Button) findViewById(R.id.budies);
+        budies.setOnClickListener(MainActivity.this);
+        insurance = (Button) findViewById(R.id.insurance);
+        insurance.setOnClickListener(MainActivity.this);
+        guideline = (Guideline) findViewById(R.id.guideline);
+        guideline2 = (Guideline) findViewById(R.id.guideline2);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.call_for_help) {
+
+        } else if (view.getId() == R.id.budies) {
+
+        } else if (view.getId() == R.id.insurance) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/sokool/isafe/Splash.java
@@ -0,0 +1,38 @@
+package com.sokool.isafe;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class Splash extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash.this,MainActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, 2000);
+
+    }
+
+
+}
